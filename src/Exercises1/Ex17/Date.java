8cac3f7c75cccc13e5709399f3c81a167e609027
Demarcily <60388907+Demarcily@users.noreply.github.com>
2021-09-24 17:13:55 +0200

    Ex 16 and 17

---
+++ src/Exercises1/Ex17/Date.java
@@ -0,0 +1,55 @@
+package Exercises1.Ex17;
+
+public class Date {
+  private int day;
+  private int month;
+  private int year;
+
+
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public int getDay() {
+    return day;
+  }
+
+  public int getMonth() {
+    return month;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setDay(int day) {
+    this.day = day;
+  }
+
+  public void setMonth(int month) {
+    this.month = month;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public void setDate(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public String toString() {
+    if (month >9) {
+      return "0" + day + "/" + month + "/" + year;
+    }
+    else {
+      return "0" + day + "/0" + month + "/" + year;
+    }
+  }
+
+
+}
